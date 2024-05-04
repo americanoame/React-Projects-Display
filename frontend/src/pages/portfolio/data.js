7294ebcd2a2792745dcc08b2a4a410f3d86172fb
Americano.Dev <andrenations@gmail.com>
2024-05-04 19:08:45 -0400

    added data.js

---
+++ frontend/src/pages/portfolio/data.js
@@ -0,0 +1,49 @@
+import Image1 from  '../../assets/RUT.png';
+import Image2 from '../../assets/MDV.png';
+import Image3 from '../../assets/Google.png';
+import Image4 from '../../assets/redux-toolkit.png';
+
+const data = [{
+    id: 1,
+    category: 'frontend',
+    image: Image1,
+    title: 'Rutgers University Gruop Project',      
+    desc: 'A social media platform for students to connect and share their experiences.',
+    demo: 'https://community-chat.herokuapp.com/',
+    github: 'https://github.com/americanoame/Community-Chat-1',
+},
+
+
+{
+    id: 4,
+    category: 'frontend',
+    image: Image4,
+    title: 'frontend',
+    desc: 'React Redux Thunk cart',
+    demo: 'https://github.com/americanoame/Redux-Thunk-Sopping-Cart/tree/main',
+    github: 'https://mdv-controller.herokuapp.com/',
+    
+},
+
+{
+    id: 2,
+    category: 'backend',
+    image: Image2,
+    title: 'backend',
+    desc: 'MDV-controller-Tech-Blog',
+    demo: 'https://github.com/americanoame/MDV-controller-Tech-Blog',
+    github: 'https://mdv-controller.herokuapp.com/',
+    
+},
+{
+    id: 3,
+    category: 'backend',
+    image: Image3,
+    title: 'backend',
+    desc: 'MERN-Book-Search-Engine',
+    demo: 'https://github.com/americanoame/MERN-Book-Search-Engine',
+    github: 'https://andre-book-search.herokuapp.com/',   
+}
+]
+
+export default data;
