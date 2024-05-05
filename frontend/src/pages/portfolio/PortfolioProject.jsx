f19b7d2eaf8d04748c70fe7ab97ea5b21ee3505f
Americano.Dev <andrenations@gmail.com>
2024-05-04 20:09:02 -0400

    added the portfolio section container

---
+++ frontend/src/pages/portfolio/PortfolioProject.jsx
@@ -0,0 +1,15 @@
+import './portfolio.css';
+
+
+const PortfolioProject = () => {
+  return (
+    <section id='portfolio'>
+        <h2>Recent Projects</h2>
+        <div className="container portfolio__container">
+
+        </div>
+    </section>
+  )
+}
+
+export default PortfolioProject
