9a06e3eecfffdbd5cd78f8ec0d8375f053a27dec
Americano.Dev <andrenations@gmail.com>
2024-06-05 19:46:07 -0400

    fixed the data and added the images

--- frontend/src/pages/portfolio/data.js
+++ frontend/src/pages/portfolio/data.js
@@ -1,31 +1,30 @@
-import Image1 from  '../../assets/RUT.png';
+import Image1 from '../../assets/RUT.png';
 import Image2 from '../../assets/MDV.png';
 import Image3 from '../../assets/Google.png';
 import Image4 from '../../assets/redux-toolkit.png';
 
-const data = [{
+const data = [
+  {
     id: 1,
     category: 'frontend',
     image: Image1,
-    title: 'Rutgers University Gruop Project',      
+    title: 'Rutgers University Gruop Project',
     desc: 'A social media platform for students to connect and share their experiences.',
     demo: 'https://community-chat.herokuapp.com/',
     github: 'https://github.com/americanoame/Community-Chat-1',
-},
+  },
 
-
-{
+  {
     id: 4,
     category: 'frontend',
     image: Image4,
     title: 'frontend',
     desc: 'React-Redux-Thunk-cart',
     demo: 'https://github.com/americanoame/Redux-Thunk-Sopping-Cart/tree/main',
-    github: 'https://mdv-controller.herokuapp.com/',
-    
-},
+    github: 'https://github.com/americanoame/Redux-Thunk-Sopping-Cart/tree/main',
+  },
 
-{
+  {
     id: 2,
     category: 'backend',
     image: Image2,
@@ -33,17 +32,16 @@ const data = [{
     desc: 'MDV-controller-Tech-Blog',
     demo: 'https://github.com/americanoame/MDV-controller-Tech-Blog',
     github: 'https://mdv-controller.herokuapp.com/',
-    
-},
-{
+  },
+  {
     id: 3,
     category: 'backend',
     image: Image3,
     title: 'backend',
     desc: 'MERN-Book-Search-Engine',
-    demo: 'https://github.com/americanoame/MERN-Book-Search-Engine',
-    github: 'https://andre-book-search.herokuapp.com/',   
-}
-]
+    demo: 'https://andre-book-search.herokuapp.com',
+    github: 'https://github.com/americanoame/MERN-Book-Search-Engine',
+  },
+];
 
 export default data;
