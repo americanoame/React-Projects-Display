17a35530fb3ca4cc14110c55aea3cf9ffd747776
Americano.Dev <andrenations@gmail.com>
2024-08-24 11:41:36 -0400

    fix lines

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/React-Projects-Display/',
   plugins: [react()],
-  base: '/React-Projects-Display/'
+  
 })
