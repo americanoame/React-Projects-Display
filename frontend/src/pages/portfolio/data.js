6ba40c135679d72887a31d163b8863d0dee42e8d
Americano.Dev <andrenations@gmail.com>
2024-08-25 11:08:28 -0400

    - Added new images to showcase additional projects in the portfolio. - Expanded the data array to include new frontend and backend projects with relevant details, demos, and GitHub links. - Updated descriptions and titles for clarity and consistency.

--- frontend/src/pages/portfolio/data.js
+++ frontend/src/pages/portfolio/data.js
@@ -1,43 +1,76 @@
 import Image1 from '../../assets/RUT.png';
-import Image2 from '../../assets/MDV.png';
-import Image3 from '../../assets/Google.png';
-import Image4 from '../../assets/redux-toolkit.png';
+import Image2 from '../../assets/portfolio.png';
+import Image3 from '../../assets/work-day.png';
+import Image4 from '../../assets/MDV.png';
+import Image5 from '../../assets/subscription.png';
+import Image6 from '../../assets/card.png';
+import Image7 from '../../assets/Google.png';
 
 const data = [
   {
     id: 1,
     category: 'frontend',
     image: Image1,
-    title: 'Rutgers University Gruop Project',
+    title: 'Rutgers University Group Project',
     desc: 'A social media platform for students to connect and share their experiences.',
     demo: 'https://community-chat.herokuapp.com/',
     github: 'https://github.com/americanoame/Community-Chat-1',
   },
 
   {
-    id: 4,
-    category: 'frontend',
-    image: Image4,
-    title: 'frontend',
-    desc: 'React-Redux-Thunk-cart',
-    demo: 'https://github.com/americanoame/Redux-Thunk-Sopping-Cart/tree/main',
-    github: 'https://github.com/americanoame/Redux-Thunk-Sopping-Cart/tree/main',
-  },
-
-  {
     id: 2,
-    category: 'backend',
+    category: 'frontend',
     image: Image2,
-    title: 'backend',
-    desc: 'MDV-controller-Tech-Blog',
-    demo: 'https://github.com/americanoame/MDV-controller-Tech-Blog',
-    github: 'https://mdv-controller.herokuapp.com/',
+    title: 'React Portfolio',
+    desc: 'This React portfolio highlights my work and demonstrates my skills across various projects.',
+    demo: 'https://americanoame.github.io/React-Portfolio-2024/',
+    github: 'https://github.com/americanoame/React-Portfolio-2024',
   },
   {
     id: 3,
-    category: 'backend',
+    category: 'frontend',
     image: Image3,
-    title: 'backend',
+    title: 'Work Day Scheduler',
+    desc: 'An application that allows users to schedule appointments and receive alerts at any time.',
+    demo: 'https://americanoame.github.io/React-typescript-product-card/',
+    github: 'https://github.com/americanoame/React-typescript-product-card',
+  },
+
+  {
+    id: 4,
+    category: 'backend',
+    image: Image4,
+    title: 'MDV-controller-Tech-Blog',
+    desc: 'MDV-controller-Tech-Blog',
+    demo: 'https://mdv-controller.herokuapp.com/',
+    github: 'https://github.com/americanoame/MDV-controller-Tech-Blog',
+  },
+
+  {
+    id: 5,
+    category: 'frontend',
+    image: Image5,
+    title: 'Subscription Manager',
+    desc: 'This app provides users with an easy way to manage their subscriptions. Users can opt to cancel or keep their subscriptions.',
+    demo: 'https://americanoame.github.io/React-Typescript-Subscription/',
+    github: 'https://github.com/americanoame/React-Typescript-Subscription',
+  },
+
+  {
+    id: 6,
+    category: 'frontend',
+    image: Image6,
+    title: 'Products Card Slider',
+    desc: 'This project showcases a responsive slider card implemented using React, HTML, and CSS.',
+    demo: 'https://americanoame.github.io/React-typescript-product-card/',
+    github: 'https://github.com/americanoame/React-typescript-product-card',
+  },
+
+  {
+    id: 7,
+    category: 'backend',
+    image: Image7,
+    title: 'MERN-Book-Search-Engine',
     desc: 'MERN-Book-Search-Engine',
     demo: 'https://andre-book-search.herokuapp.com',
     github: 'https://github.com/americanoame/MERN-Book-Search-Engine',
